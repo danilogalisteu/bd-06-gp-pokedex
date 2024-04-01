d240f8c25a7c8e89b5412ad5349e4d2902bd87ba
danilogalisteu <danilogalisteu@gmail.com>
2024-04-01 10:53:06 -0300

    new cache types

---
+++ pokecache.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	cache map[string]cacheEntry
+	lock  *sync.Mutex
+}
