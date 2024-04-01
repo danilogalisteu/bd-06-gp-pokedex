3f7e2849647e46ca55e3ecf9d3de80edc3b650b0
danilogalisteu <danilogalisteu@gmail.com>
2024-04-01 11:02:24 -0300

    added cache methods to add and get entry

--- pokecache.go
+++ pokecache.go
@@ -14,3 +14,19 @@ type Cache struct {
 	cache map[string]cacheEntry
 	lock  *sync.Mutex
 }
+
+func (c Cache) Add(key string, val []byte) {
+	c.lock.Lock()
+	c.cache[key] = cacheEntry{time.Now(), val}
+	c.lock.Unlock()
+}
+
+func (c Cache) Get(key string) ([]byte, bool) {
+	c.lock.Lock()
+	entry, ok := c.cache[key]
+	c.lock.Unlock()
+	if !ok {
+		return nil, false
+	}
+	return entry.val, true
+}
